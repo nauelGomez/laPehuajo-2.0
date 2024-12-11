f9de984e11bcb6a6ae6a0caf6c4e57953adea97c
nauelGomez <139215988+nauelGomez@users.noreply.github.com>
2024-12-11 10:38:41 -0300

    login ok

--- src/app/paginas/login/login.component.ts
+++ src/app/paginas/login/login.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  email: string = '';
+  password: string = '';
+  errorMessage: string | null = null;
+  constructor(private router: Router) {}
+  onSubmit(): void {
+    if (this.email === 'lapehuajo@admin.com' && this.password === 'admin123') {
+      alert('¡Inicio de sesión exitoso!');
+      this.router.navigate(['/admin-panel']);
+      this.errorMessage = null;
+    } else {
+      this.errorMessage = 'Correo electrónico o contraseña incorrectos.';
+    }
+  }
 }
